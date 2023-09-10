b858c59cbe2d20a246ecc36bd6d8ccad806bd30c
Olzhas Ilyassov <olzhas.ilyassov@alg-in.kz>
2023-09-11 03:04:43 +0600

    setup language.Tag

--- cmd/www/hanlers.go
+++ cmd/www/hanlers.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	// Extract the locale from the URL path.
 	locale := r.URL.Query().Get(":locale")
 
+	// Declare variable to hold the target language tag.
+	var lang language.Tag
+
 	switch locale {
-	case "en-gb", "de-de", "fr-ch":
-		fmt.Fprintf(w, "The locale is %s\n", locale)
+	case "en-gb":
+		lang = language.MustParse("en-GB")
+	case "de-de":
+		lang = language.MustParse("de-DE")
+	case "fr-ch":
+		lang = language.MustParse("fr-CH")
 	default:
 		http.NotFound(w, r)
+		return
 	}
+
+	// Initialize a message.Printer which uses the target language.
+	p := message.NewPrinter(lang)
+	// Print the welcome message translated into the target language.
+	p.Fprintf(w, "Welcome!\n")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bookstore.example.com
 
 go 1.20
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+	golang.org/x/text v0.13.0
+)
