726f84519f738623e97b144316ca7bf9e627da68
Olzhas Ilyassov <olzhas.ilyassov@alg-in.kz>
2023-09-11 03:52:48 +0600

    add localizer abstraction

--- cmd/www/handlers.go
+++ cmd/www/handlers.go
@@ -1,41 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	// Import the internal/translations package, so that its init() function is called.
-	_ "bookstore.example.com/internal/translations"
-
-	"golang.org/x/text/language"
-	"golang.org/x/text/message"
+	"bookstore.example.com/internal/localizer" // New import
 )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	// Extract the locale from the URL path.
-	locale := r.URL.Query().Get(":locale")
-
-	// Declare variable to hold the target language tag.
-	var lang language.Tag
-
-	switch locale {
-	case "en-gb":
-		lang = language.MustParse("en-GB")
-	case "de-de":
-		lang = language.MustParse("de-DE")
-	case "fr-ch":
-		lang = language.MustParse("fr-CH")
-	default:
+	// Initialize a new Localizer based on the locale ID in the URL.
+	l, ok := localizer.Get(r.URL.Query().Get(":locale"))
+	if !ok {
 		http.NotFound(w, r)
 		return
 	}
 
 	var totalBookCount = 1_252_794
 
-	// Initialize a message.Printer which uses the target language.
-	p := message.NewPrinter(lang)
-	// Print the welcome message translated into the target language.
-	p.Fprintf(w, "Welcome!\n")
+	// Update these to use the new Translate() method.
+	fmt.Fprintln(w, l.Translate("Welcome!"))
+	fmt.Fprintln(w, l.Translate("%d books available", totalBookCount))
 
-	// Example of translated message with enterpolated integer value.
-	p.Fprintf(w, "%d books available\n", totalBookCount)
+	// Add an additional "Launching soon!" message.
+	fmt.Fprintln(w, l.Translate("Launching soon!"))
 }
